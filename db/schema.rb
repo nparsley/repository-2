d22c96c3c376570651a71725aac01d1389b23eac
Nick Parsley <nicholasparsley@gmail.com>
2019-09-19 01:11:51 +0000

    Add a comment

--- db/schema.rb
+++ db/schema.rb
@@ -29,8 +29,11 @@ ActiveRecord::Schema.define(version: 2019_09_16_015112) do
   create_table "photos", force: :cascade do |t|
     t.integer "place_id"
     t.text "caption"
+    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["place_id"], name: "index_photos_on_place_id"
+    t.index ["user_id", "place_id"], name: "index_photos_on_user_id_and_place_id"
   end
 
   create_table "places", force: :cascade do |t|
@@ -51,6 +54,11 @@ ActiveRecord::Schema.define(version: 2019_09_16_015112) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet "current_sign_in_ip"
+    t.inet "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true

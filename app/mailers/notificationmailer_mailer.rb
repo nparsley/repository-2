52a973a558b9ba426f2d132c241245279b788e3f
Nick Parsley <nicholasparsley@gmail.com>
2019-09-29 16:17:20 +0000

    Mailing on production

--- app/mailers/notificationmailer_mailer.rb
+++
@@ -1,7 +0,0 @@
-class NotificationMailer < ApplicationMailer
-  default from: "no-reply@nomsterapp.com"
-
-  def comment_added
-    mail(to: "nicholasparsley@gmail.com",
-         subject: "A comment has been added to your place")
-  end
